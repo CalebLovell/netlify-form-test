91166d101124b218bd647b3b59329e5f9deee7ad
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-14 17:27:26 -0600

    initial commit to test form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello. This is a form test for Netlify because it's broken on my portfolio!
+        <section className="contact" id="contact">
+          <h1 className="title">Contact</h1>
+          <p>Have a question or just want to say hi?</p>
+          <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+            onsubmit="return false"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <input
+              type="hidden"
+              name="bot-field"
+              value="Don't fill this out if you're a human."
+            />
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <textarea name="message" placeholder="Message" />
+            <div>
+              <button type="submit">SEND</button>
+            </div>
+          </form>
+        </section>
       </header>
     </div>
   );
